6536e1a845eb176f015bd7e773827592572e1d41
Mark Tickner <mark.tickner@live.co.uk>
2015-02-13 15:45:09 +0000

    Fixed login errors and student dashboard

--- login-check.php
+++ login-check.php
@@ -5,13 +5,13 @@
 // Source: http://board.phpbuilder.com/showthread.php?10263469#post10986948
 if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
     // File requested directly, redirect to home page
-    header('Location: index.php');
+    header('Location: /codezero/index.php');
 } else {
     // Checks user is logged in and redirects if necessary
     if (!isset($_SESSION['currentUser'])) {
         // User is not logged in
         // Redirect to home page
-        header('Location: index.php');
+        header('Location: /codezero/index.php');
     }
 }
 ?>
