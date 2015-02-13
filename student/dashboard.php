74d58b2a546a66d723a9f77e1b5c5c93a0a1ab89
Mark Tickner <tm112@greenwich.ac.uk>
2015-02-13 15:59:48 +0000

    Working copy of the dashboard

--- student/dashboard.php
+++ student/dashboard.php
@@ -73,7 +73,7 @@ $supervisor = $u->getResponse();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--<link href="css/styles.css" rel="stylesheet" type="text/css"/>-->
-    <link href="../1/Styledcommunication/css/styles.css" rel="stylesheet" type="text/css"/>
+    <link href="../css/styles.css" rel="stylesheet" type="text/css"/>
     <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="../js/materialize.min.js"></script>
