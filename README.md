a02ee809e5468993eb6602777ae147647f476a33
Aaron Dunphy <da236@greenwich.ac.uk>
2015-02-19 11:01:42 +0000

    Re uploaded master due to wrong merge

---
+++ README.md
@@ -0,0 +1,24 @@
+# eSupervisionSystem
+
+Master link: http://stuiis.cms.gre.ac.uk/codezero/
+
+<p>
+<b>Students:</b>
+<br />
+Active = tm112, da236, la234, tb117, bd451, la345, bl332
+<br />
+Not active = jd119, kb528
+</p>
+
+<p>
+<b>Staff:</b>
+<br />
+
+Active + authorised = pm253, md238, la790
+<br />
+Active + not authorised = tm21, dl45, jm197, kp666
+<br />
+Not active + authorised = sa112
+<br />
+Not active + not authorised = dr456
+</p>
---
+++ database-connection.php
@@ -0,0 +1,57 @@
+<?php
+// Database related functions
+
+// Prevent direct access to this file
+// Source: http://board.phpbuilder.com/showthread.php?10263469#post10986948
+if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
+    // File requested directly, redirect user
+    header('Location: http://stuiis.cms.gre.ac.uk/codezero/');
+} else {
+    // Function to create a database connection
+    function GetConnection() {
+        require 'mysql.php';
+
+        // Return database link
+        return mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+    }
+
+    // Function to close the database connection
+    function CloseConnection($link) {
+        mysqli_close($link);
+    }
+
+    // Function to authenticate a user
+    function LoginUser($link, $username, $password) {
+        // Get student password hash from database
+        $studentResult = mysqli_query($link, "SELECT student_password FROM esuper_student WHERE student_username = '$username'");
+        $studentPasswordHash = mysqli_fetch_assoc($studentResult)['student_password'];
+
+        // Get staff password hash from database
+        $staffResult = mysqli_query($link, "SELECT staff_password FROM esuper_staff WHERE staff_username = '$username'");
+        $staffPasswordHash = mysqli_fetch_assoc($staffResult)['staff_password'];
+
+        if (mysqli_num_rows($studentResult) == 1) {
+            // User found in student table
+            if (password_verify($password, $studentPasswordHash)) {
+                $sql = "SELECT student_id, student_first, student_last, student_username, student_banner_id, student_active AS is_active FROM esuper_student WHERE student_username = '$username'";
+                $result = mysqli_query($link, $sql);
+                $student = mysqli_fetch_assoc($result);
+                $student += array('user_type' => 'student');
+
+                return $student;
+            }
+        } else if (mysqli_num_rows($staffResult) == 1) {
+            // User found in staff table
+            if (password_verify($password, $staffPasswordHash)) {
+                $sql = "SELECT staff_id, staff_first, staff_last, staff_username, staff_banner_id, staff_active AS is_active, staff_authorised FROM esuper_staff WHERE staff_username = '$username'";
+                $result = mysqli_query($link, $sql);
+                $staff = mysqli_fetch_assoc($result);
+                $staff += array('user_type' => 'staff');
+
+                return $staff;
+            }
+        } else {
+            return null;
+        }
+    }
+}
