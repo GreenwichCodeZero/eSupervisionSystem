e57d21f1cc98fe78f611e6ac8fc8d620700ab5ae
Aaron Dunphy <da236@greenwich.ac.uk>
2015-02-22 12:50:45 +0000

    Improved staff dashboard

--- classes/userDetails.class.php
+++ classes/userDetails.class.php
@@ -120,6 +120,92 @@ AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
         $this->response($row);
     }
 
+
+    public function supervisorStudents ($user_id) {
+        // $s = new Security (); 
+        //     try { $this->con = $s->db (); }
+
+        //     catch (Exception $e) {
+        //         echo $e->getMessage ();
+        //         exit;
+        //     }
+
+        $result = $this->con->prepare(
+            'SELECT * FROM esuper_user_allocation, esuper_staff, esuper_student WHERE esuper_user_allocation.supervisor_id = ' . $user_id . ' AND esuper_user_allocation.supervisor_id = esuper_staff.staff_id AND  esuper_user_allocation.student_id = esuper_student.student_id');
+        $result->bindValue(':student_id', $student_id);
+        try {
+            $result->execute();
+        }
+
+        catch (PDOException $e) {
+            echo "ERROR:";
+            echo "\n\n\r\r". $e->getMessage ();
+            exit;
+        }
+
+        $row = $result->fetchAll();
+        $result = null;
+        $this->response ($row);
+    }
+
+        public function noSupervisor () {
+        // $s = new Security (); 
+        //     try { $this->con = $s->db (); }
+
+        //     catch (Exception $e) {
+        //         echo $e->getMessage ();
+        //         exit;
+        //     }
+
+        $result = $this->con->prepare(
+'SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last from esuper_student WHERE esuper_student.student_id NOT IN (SELECT student_id from esuper_user_allocation) UNION SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last FROM esuper_user_allocation, esuper_student WHERE esuper_user_allocation.student_id = esuper_student.student_id AND esuper_user_allocation.supervisor_id IS NULL ');
+       // $result->bindValue();
+        try {
+            $result->execute();
+        }
+
+        catch (PDOException $e) {
+            echo "ERROR:";
+            echo "\n\n\r\r". $e->getMessage ();
+            exit;
+        }
+
+        $row = $result->fetchAll();
+        $result = null;
+        $this->response ($row);
+    }
+
+
+    public function noSecondMarker () {
+        // $s = new Security (); 
+        //     try { $this->con = $s->db (); }
+
+        //     catch (Exception $e) {
+        //         echo $e->getMessage ();
+        //         exit;
+        //     }
+
+        $result = $this->con->prepare(
+'SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last from esuper_student WHERE esuper_student.student_id NOT IN (SELECT student_id from esuper_user_allocation) UNION SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last FROM esuper_user_allocation, esuper_student WHERE esuper_user_allocation.student_id = esuper_student.student_id AND esuper_user_allocation.second_id IS NULL ');
+  //      $result->bindValue();
+        try {
+            $result->execute();
+        }
+
+        catch (PDOException $e) {
+            echo "ERROR:";
+            echo "\n\n\r\r". $e->getMessage ();
+            exit;
+        }
+
+        $row = $result->fetchAll();
+        $result = null;
+        $this->response ($row);
+    }
+
+
+
+
     public function getResponse() {
         return $this->response;
     }
