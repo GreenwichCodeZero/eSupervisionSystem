f0daba474f196afd9269cbfc830f0a0701afca21
Aaron Dunphy <da236@greenwich.ac.uk>
2015-02-25 09:08:16 +0000

    Added search page

---
+++ staff/seach.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>eSupervision - Meetings</title>
+
+    <meta name="author" content="Code Zero"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="../css/styles.css" rel="stylesheet" type="text/css"/>
+    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection"/>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="../js/materialize.min.js"></script>
+</head>
+<body>
+
+<nav>
+    <div class="nav-wrapper green">
+        <ul id="nav-mobile" class="side-nav">
+            <li>
+                <a href="index.php">Dashboard</a>
+            </li>
+            <li>
+                <a href="meetings.php">Meetings</a>
+            </li>
+            <li>
+                <a href="messages.php">Messages</a>
+            </li>
+            <li>
+                <a href="blogs.php">Blog</a>
+            </li>
+            <li>
+                <a href="uploads.php">Uploads</a>
+            </li>
+        </ul>
+        <a class="button-collapse" href="#" data-activates="nav-mobile"><i class="mdi-navigation-menu"></i></a>
+    </div>
+</nav>
+
+<div class="container">
