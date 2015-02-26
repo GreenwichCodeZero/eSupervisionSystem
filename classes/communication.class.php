e1f132aa560edd46fb764c77307c8d7f4281ef18
Mark Tickner <mark.tickner@live.co.uk>
2015-02-26 12:09:29 +0000

    Some small changes for US21
    Merging now to hopefully avoid conflicts when others create new
    branches. Ignore most of the red, unfortunately when my IDE reformats
    code it confuses GitHub with lots of changes to white space characters…

--- classes/communication.class.php
+++ classes/communication.class.php
@@ -13,128 +13,117 @@ class Communication {
 // * @authors       Dwayne Brown & Mark Tickner 
 // * @version    	1.1
 
-	private $action;
-	private $response;
-	private $con;
+    private $action;
+    private $response;
+    private $con;
 
-	private $communication_id;
-	private $from;
-	private $to;
-	private $date_addded = '';
-	private $time_addded ='';
-	private $body;
-	private $communication_type;
+    private $communication_id;
+    private $from;
+    private $to;
+    private $date_addded = '';
+    private $time_addded = '';
+    private $body;
+    private $communication_type;
 
-	// Load variables from POST into object 
-	public function __construct ( ) {
+    // Load variables from POST into object
+    public function __construct() {
+        date_default_timezone_set('Europe/London');
 
-	    date_default_timezone_set('Europe/London'); 
+        $s = new Security ();
+        $s->clean($_POST);
+        try {
+            $this->con = $s->db();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            exit;
+        }
+    }
 
-		$s = new Security (); 
-			$s->clean ($_POST);
-			try { $this->con = $s->db (); }
+    public function insert($from_user) {
+        try {
+            $this->validate();
+        } catch (Exception $e) {
+            throw new Exception ($e->getMessage());
+            exit;
+        }
 
-			catch (Exception $e) {
-				echo $e->getMessage ();
-				exit;
-			}
+        $this->from = $from_user;
 
-	}
+        $this->action = (isset ($_POST['communication_action'])) ? $_POST['communication_action'] : null;
 
-	public function insert ($from_user) {
+        switch ($this->action) {
+            case 'sendmessage':
+                // echo "message:";
+                $this->add('message');
+                break;
 
-		try { $this->validate (); }
+            case 'posttoblog':
+                // echo "Blog Post: id1";
+                $this->add('blog');
+                break;
 
-		catch (Exception $e) {
-			throw new Exception ($e->getMessage());
-			exit;
-		}
+            default:
+                // echo "nothing to do <pre>";
+                print_r($_POST);
+                exit;
+                break;
+        }
+    }
 
-		$this->from = $from_user;
+    public function validate() {
+        foreach ($_POST as $key => $value) {
+            if (empty ($value)) {
+                throw new Exception ('Please complete all fields: ' . $key . ' was empty');
+                exit;
+            }
+        }
+    }
 
-		$this->action = ( isset ($_POST['communication_action']) ) ? $_POST['communication_action'] : null;
+    // Add a new communication to the database
+    public function add($type) {
+        switch ($type) {
+            case 'message':
+                $this->type = 2;
+                $this->to = $_POST ['communication_to_id'];
+                break;
 
-		switch ( $this->action ) {
-			case 'sendmessage':
-				// echo "message:";
-				$this->add ('message');
-			break;
+            case 'blog':
+                $this->type = 1;
+                $this->to = 0;
+                break;
 
-			case 'posttoblog':
-				// echo "Blog Post: id1";
-				$this->add ('blog');
-			break;
+            default:
+                echo "<br>NO POST VALUES<br>";
+                print_r($_POST);
+                exit;
+                break;
+        }
 
-			default: 
-				// echo "nothing to do <pre>";
-				print_r ($_POST);
-				exit;
-			break;
+        $this->file_id = 0;
 
-		}
+        if ($_FILES['fileToUpload']['size'] > 0) {
 
-	}
+            $f = new File ();
+            try {
+                $f->add($this->from);
+            } catch (Exception $e) {
+                echo $e->getMessage();
+                exit;
+            }
 
-	public function validate () {
+            $this->file_id = $f->getResponse();
 
-		foreach($_POST as $key => $value) {
- 			if (empty ($value)) {
- 				throw new Exception ('Please complete all fields: '.$key.' was empty');
- 				exit;
- 			}
-		}
+            $this->response('file uploaded: ' . $this->file_id);
+        }
 
-	}
+        $this->from = $_POST ['communication_from_id'];
+        $this->date_addded = time();
+        $this->time_addded = time();
+        $this->body = $_POST ['communication_body'];
 
-	// Add a new communication to the database
-	public function add ( $type) {
-
-
-		switch ( $type ) {
-			case 'message':
-				$this->type = 2;
-				$this->to = $_POST ['communication_to_id'];
-			break;
-
-			case 'blog':
-				$this->type = 1;
-				$this->to = 0;
-			break;
-
-			default: 
-				echo "<br>NO POST VALUES<br>";
-				print_r ($_POST);
-				exit;
-			break;
-
-		}
-
-		$this->file_id = 0;
-
-		     if($_FILES['fileToUpload']['size'] > 0) {
-
-		        $f = new File ();
-		        try { $f->add ($this->from); }
-		        catch (Exception $e){
-		        	echo $e->getMessage ();
-		        	exit;
-		        }
-
-		        $this->file_id = $f->getResponse ();
-
-		        $this->response ('file uploaded: '.$this->file_id);
-		    	
-	    	}
-
-		$this->from = $_POST ['communication_from_id'];
-		$this->date_addded = time();
-		$this->time_addded = time();
-		$this->body = $_POST ['communication_body'];
-
-		$result = $this->con->prepare(
-			"INSERT INTO `esuper_communication` (communication_from_id,communication_to_id, communication_date_added, communication_time_added, communication_type_id, communication_body, communication_file_id)
-			VALUES 
-			(:from_id,:to_id,:date_added,:time_added,:type_id ,:comm_body, :comm_file_id);");
+        $result = $this->con->prepare(
+            "INSERT INTO `esuper_communication` (communication_from_id,communication_to_id, communication_date_added, communication_time_added, communication_type_id, communication_body, communication_file_id)
+			VALUES (:from_id,:to_id,:date_added,:time_added,:type_id ,:comm_body, :comm_file_id);");
 
         $result->bindValue(':from_id', $this->from);
         $result->bindValue(':to_id', $this->to);
@@ -144,12 +133,10 @@ class Communication {
         $result->bindValue(':comm_body', $this->body);
         $result->bindValue(':comm_file_id', $this->file_id);
         try {
-        	$result->execute();
-        }
-
-        catch (PDOException $e) {
-        	echo "\n\n\r\r". $e->getMessage ();
-        	exit;
+            $result->execute();
+        } catch (PDOException $e) {
+            echo "\n\n\r\r" . $e->getMessage();
+            exit;
         }
 
         // $row = $result->fetch(PDO::FETCH_ASSOC);
@@ -160,23 +147,21 @@ class Communication {
             'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
 
-        if ( mail ('bd118@greenwich.ac.uk, '.$this->from.'@greenwich.ac.uk',
-        	'New Message Received', 'A new message was submitted and is waiting for you on the eSupervision System.', $headers) ){
-               $this->response ('Your content was commited successfully and your tutor has been notified .');
+        if (mail('bd118@greenwich.ac.uk, ' . $this->from . '@greenwich.ac.uk',
+            'New Message Received', 'A new message was submitted and is waiting for you on the eSupervision System.', $headers)) {
+            $this->response('Your content was commited successfully and your tutor has been notified .');
         } else {
-               $this->response ('Your content was commited successfully but your tutor could not be notified.');
+            $this->response('Your content was commited successfully but your tutor could not be notified.');
         }
+    }
 
-	}
-
-	// Find a comment by comment id, type, who posted etc.
-	public function getAll ( $type, $user, $user_type = null) { 
-
-		switch ($type) {
-			case 'blog': 
-				$this->type_id = 1;
-				$this->result = $this->con->prepare(
-						'SELECT 
+    // Find a comment by comment id, type, who posted etc.
+    public function getAll($type, $user, $user_type = null) {
+        switch ($type) {
+            case 'blog':
+                $this->type_id = 1;
+                $this->result = $this->con->prepare(
+                    'SELECT
 						`esuper_communication`.`communication_id`
 						, `esuper_communication`.`communication_body`
 						, `esuper_communication`.`communication_date_added`
@@ -185,16 +170,16 @@ class Communication {
 						FROM
 						`esuper_communication`
 						 WHERE 
-						`esuper_communication`.`communication_type_id` ='.$this->type_id);
-			break;
+						`esuper_communication`.`communication_type_id` =' . $this->type_id);
 
-			case 'message':
-				$this->type_id = 2;
-				switch ($user_type) {
+                break;
+            case 'message':
+                $this->type_id = 2;
+                switch ($user_type) {
 
-					case 'staff';
-					$this->result = $this->con->prepare(
-						'SELECT 
+                    case 'staff';
+                        $this->result = $this->con->prepare(
+                            'SELECT
 						`esuper_communication`.`communication_id`
 						, `esuper_communication`.`communication_body`
 						, `esuper_communication`.`communication_date_added`
@@ -206,17 +191,17 @@ class Communication {
 						`esuper_communication`,
 						`esuper_student`
 						 WHERE 
-						`esuper_communication`.`communication_type_id` ='.$this->type_id.'
+						`esuper_communication`.`communication_type_id` =' . $this->type_id . '
 						AND 
-						`esuper_communication`.`communication_from_id` = "'.$user.'"
+						`esuper_communication`.`communication_from_id` = "' . $user . '"
 						AND 
-						`esuper_student`.`student_username` = `esuper_communication`.`communication_to_id`'	
-					);
-					break;
+						`esuper_student`.`student_username` = `esuper_communication`.`communication_to_id`'
+                        );
 
-					case 'student':
-						$this->result = $this->con->prepare(
-						'SELECT 
+                        break;
+                    case 'student':
+                        $this->result = $this->con->prepare(
+                            'SELECT
 						`esuper_communication`.`communication_id`
 						, `esuper_communication`.`communication_body`
 						, `esuper_communication`.`communication_date_added`
@@ -228,42 +213,39 @@ class Communication {
 						`esuper_communication`,
 						`esuper_staff`
 						 WHERE 
-						`esuper_communication`.`communication_type_id` ='.$this->type_id.'
+						`esuper_communication`.`communication_type_id` =' . $this->type_id . '
 						AND 
-						`esuper_communication`.`communication_from_id` = "'.$user.'"
+						`esuper_communication`.`communication_from_id` = "' . $user . '"
 						AND 
-						`esuper_staff`.`staff_username` = `esuper_communication`.`communication_to_id`'	
-					);
-					break;
-			}
-			break;
-		}
-		
-		
+						`esuper_staff`.`staff_username` = `esuper_communication`.`communication_to_id`'
+                        );
 
-        try {
-        	$this->result->execute();
+                        break;
+                }
+
+                break;
         }
 
-        catch (PDOException $e) {
-        	echo "ERROR:";
-        	echo "\n\n\r\r". $e->getMessage ();
-        	exit;
+        try {
+            $this->result->execute();
+        } catch (PDOException $e) {
+            echo "ERROR:";
+            echo "\n\n\r\r" . $e->getMessage();
+            exit;
         }
 
         $row = $this->result->fetchAll();
         $this->result = null;
-        $this->response ($row);
-	}
+        $this->response($row);
+    }
 
-	public function received ( $user, $user_type ) { 
-		
-		$this->type_id = 2;
-				switch ($user_type) {
+    public function received($user, $user_type) {
+        $this->type_id = 2;
+        switch ($user_type) {
 
-					case 'staff';
-					$this->result = $this->con->prepare(
-						'SELECT 
+            case 'staff';
+                $this->result = $this->con->prepare(
+                    'SELECT
 						`esuper_communication`.`communication_id`
 						, `esuper_communication`.`communication_body`
 						, `esuper_communication`.`communication_date_added`
@@ -275,17 +257,17 @@ class Communication {
 						`esuper_communication`,
 						`esuper_student`
 						 WHERE 
-						`esuper_communication`.`communication_type_id` ='.$this->type_id.'
+						`esuper_communication`.`communication_type_id` =' . $this->type_id . '
 						AND 
-						`esuper_communication`.`communication_to_id` = "'.$user.'"
+						`esuper_communication`.`communication_to_id` = "' . $user . '"
 						AND 
-						`esuper_student`.`student_username` = `esuper_communication`.`communication_from_id`'	
-					);
-					break;
+						`esuper_student`.`student_username` = `esuper_communication`.`communication_from_id`'
+                );
+                break;
 
-					case 'student':
-						$this->result = $this->con->prepare(
-						'SELECT 
+            case 'student':
+                $this->result = $this->con->prepare(
+                    'SELECT
 						`esuper_communication`.`communication_id`
 						, `esuper_communication`.`communication_body`
 						, `esuper_communication`.`communication_date_added`
@@ -297,41 +279,39 @@ class Communication {
 						`esuper_communication`,
 						`esuper_staff`
 						 WHERE 
-						`esuper_communication`.`communication_type_id` ='.$this->type_id.'
+						`esuper_communication`.`communication_type_id` =' . $this->type_id . '
 						AND 
-						`esuper_communication`.`communication_to_id` = "'.$user.'"
+						`esuper_communication`.`communication_to_id` = "' . $user . '"
 						AND 
-						`esuper_staff`.`staff_username` = `esuper_communication`.`communication_from_id`'	
-					);
-					break;
-				}
-
-        try {
-        	$this->result->execute();
+						`esuper_staff`.`staff_username` = `esuper_communication`.`communication_from_id`'
+                );
+                break;
         }
 
-        catch (PDOException $e) {
-        	echo "ERROR:";
-        	echo "\n\n\r\r". $e->getMessage ();
-        	exit;
+        try {
+            $this->result->execute();
+        } catch (PDOException $e) {
+            echo "ERROR:";
+            echo "\n\n\r\r" . $e->getMessage();
+            exit;
         }
 
         $row = $this->result->fetchAll();
         $this->result = null;
-        $this->response ($row);
-	}
+        $this->response($row);
+    }
 
-	// Remove a communication from the database
-	public function remove () {}
+    // Remove a communication from the database
+    public function remove() {
+    }
 
-	public function response ( $var ) {
-		$this->response = $var;
-	}
+    public function response($var) {
+        $this->response = $var;
+    }
 
-	public function getResponse () {
-		return $this->response;
-	}
-
+    public function getResponse() {
+        return $this->response;
+    }
 }
 
 ?>
--- classes/userDetails.class.php
+++ classes/userDetails.class.php
@@ -47,12 +47,15 @@ class UserDetails {
 
     public function AllMyStudents($staff_id) {
         $result = $this->con->prepare(
-            "SELECT  `esuper_student`.`student_first` ,  `esuper_student`.`student_last` ,  `esuper_student`.`student_username`
-FROM  `esuper_student` ,  `esuper_user_allocation` ,  `esuper_staff`  
-WHERE
-`esuper_student`.`student_id` = `esuper_user_allocation`.`student_id` AND  
- `esuper_user_allocation`.`supervisor_id` =  `esuper_staff`.`staff_id`
-AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
+            'SELECT student.student_first, student.student_last, student.student_username
+             FROM esuper_student student
+             JOIN esuper_user_allocation allocation ON student.student_id = allocation.student_id
+             JOIN esuper_staff spv ON allocation.supervisor_id = spv.staff_id
+             JOIN esuper_staff snd ON allocation.second_id = snd.staff_id
+             WHERE spv.staff_id = :staff_id
+             OR snd.staff_id = :staff_id
+             ORDER BY student.student_last');
+        $result->bindValue(':staff_id', $staff_id);
 
         try {
             $result->execute();
@@ -76,7 +79,8 @@ AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
              JOIN esuper_staff spv ON allocation.supervisor_id = spv.staff_id
              JOIN esuper_staff snd ON allocation.second_id = snd.staff_id
              WHERE spv.staff_username = :staff_username
-             OR snd.staff_username = :staff_username');
+             OR snd.staff_username = :staff_username
+             ORDER BY student.student_last');
         $result->bindValue(':staff_username', $staff_username);
 
         try {
@@ -123,7 +127,7 @@ AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
     }
 
 
-    public function supervisorStudents ($user_id) {
+    public function supervisorStudents($user_id) {
         // $s = new Security (); 
         //     try { $this->con = $s->db (); }
 
@@ -137,20 +141,18 @@ AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
         $result->bindValue(':student_id', $student_id);
         try {
             $result->execute();
-        }
-
-        catch (PDOException $e) {
+        } catch (PDOException $e) {
             echo "ERROR:";
-            echo "\n\n\r\r". $e->getMessage ();
+            echo "\n\n\r\r" . $e->getMessage();
             exit;
         }
 
         $row = $result->fetchAll();
         $result = null;
-        $this->response ($row);
+        $this->response($row);
     }
 
-        public function noSupervisor () {
+    public function noSupervisor() {
         // $s = new Security (); 
         //     try { $this->con = $s->db (); }
 
@@ -160,25 +162,23 @@ AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
         //     }
 
         $result = $this->con->prepare(
-'SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last from esuper_student WHERE esuper_student.student_id NOT IN (SELECT student_id from esuper_user_allocation) UNION SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last FROM esuper_user_allocation, esuper_student WHERE esuper_user_allocation.student_id = esuper_student.student_id AND esuper_user_allocation.supervisor_id IS NULL ');
-       // $result->bindValue();
+            'SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last from esuper_student WHERE esuper_student.student_id NOT IN (SELECT student_id from esuper_user_allocation) UNION SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last FROM esuper_user_allocation, esuper_student WHERE esuper_user_allocation.student_id = esuper_student.student_id AND esuper_user_allocation.supervisor_id IS NULL ');
+        // $result->bindValue();
         try {
             $result->execute();
-        }
-
-        catch (PDOException $e) {
+        } catch (PDOException $e) {
             echo "ERROR:";
-            echo "\n\n\r\r". $e->getMessage ();
+            echo "\n\n\r\r" . $e->getMessage();
             exit;
         }
 
         $row = $result->fetchAll();
         $result = null;
-        $this->response ($row);
+        $this->response($row);
     }
 
 
-    public function noSecondMarker () {
+    public function noSecondMarker() {
         // $s = new Security (); 
         //     try { $this->con = $s->db (); }
 
@@ -188,25 +188,23 @@ AND   `esuper_staff`.`staff_id` =  '" . $staff_id . "'");
         //     }
 
         $result = $this->con->prepare(
-'SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last from esuper_student WHERE esuper_student.student_id NOT IN (SELECT student_id from esuper_user_allocation) UNION SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last FROM esuper_user_allocation, esuper_student WHERE esuper_user_allocation.student_id = esuper_student.student_id AND esuper_user_allocation.second_id IS NULL ');
-  //      $result->bindValue();
+            'SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last from esuper_student WHERE esuper_student.student_id NOT IN (SELECT student_id from esuper_user_allocation) UNION SELECT esuper_student.student_id, esuper_student.student_first, esuper_student.student_last FROM esuper_user_allocation, esuper_student WHERE esuper_user_allocation.student_id = esuper_student.student_id AND esuper_user_allocation.second_id IS NULL ');
+        //      $result->bindValue();
         try {
             $result->execute();
-        }
-
-        catch (PDOException $e) {
+        } catch (PDOException $e) {
             echo "ERROR:";
-            echo "\n\n\r\r". $e->getMessage ();
+            echo "\n\n\r\r" . $e->getMessage();
             exit;
         }
 
         $row = $result->fetchAll();
         $result = null;
-        $this->response ($row);
+        $this->response($row);
     }
 
 
-public function isStaffAuthorised($staff_id) {
+    public function isStaffAuthorised($staff_id) {
         $result = $this->con->prepare("SELECT staff_authorised FROM esuper_staff WHERE staff_id = " . '"' . $staff_id . '"');
 
         $result->bindValue(':staff_id', $staff_id);
