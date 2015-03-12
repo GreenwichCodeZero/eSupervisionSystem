6ee7a4aaa4fad3873540ed01cd4dd9e01af7da54
Aaron Dunphy <da236@greenwich.ac.uk>
2015-03-12 10:08:17 +0000

    Fixed missing nav links on allocate.php

--- staff/allocate.php
+++ staff/allocate.php
@@ -16,6 +16,12 @@ $currentStaff = $_SESSION['currentUser'];
 $staff_id = $currentStaff['staff_id'];
 $selectedStudents = $_POST['students'];
 
+
+$getStaffDetailsQ = new UserDetails ();
+$getStaffDetailsQ->isStaffAuthorised($staff_id);
+$getStaffDetails = $getStaffDetailsQ->getResponse();
+
+
 // Determine permissions of current user
 if ($currentStaff['user_type'] === 'student') {
     // Redirect to staff dashboard
@@ -334,7 +340,7 @@ if (isset($_POST['saveAllocate'])) {
                 <a href="uploads.php">Project Uploads</a>
             </li>
             <?php
-            if ($staffAuthorsied == 1) {
+            if ($getStaffDetails[0]['staff_authorised'] == 1) {
                 echo '<li><a href="search.php">Search</a></li>
                     <li><a href="viewDashboards.php">View dashboards</a></li>
                     <li><a href="reports.php">Reports</a></li>';
