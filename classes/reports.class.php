a468d7fc2cafd93b8e5c915b012fbf610393c8f5
Aaron Dunphy <da236@greenwich.ac.uk>
2015-03-12 16:13:59 +0000

    Added report
    Inactive students assigned to a supervisor

--- classes/reports.class.php
+++ classes/reports.class.php
@@ -14,6 +14,25 @@ class Reports {
         }
     }
 
+public function notActiveButAssignedToASupervisor() {
+        $result = $this->con->prepare(
+            'SELECT * FROM esuper_student s, esuper_user_allocation ua
+            WHERE s.student_id = ua.student_id
+            AND s.student_active = 0
+               ');
+
+        try {
+            $result->execute();
+        } catch (PDOException $e) {
+            echo "ERROR:";
+            echo "\n\n\r\r" . $e->getMessage();
+            exit;
+        }
+
+        $row = $result->fetchAll();
+        $result = null;
+        $this->response($row);
+    }
 
 public function notLoggedIn7Days() {
         $result = $this->con->prepare(
