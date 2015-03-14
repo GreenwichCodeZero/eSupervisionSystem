6111f9481a2caa6e60682a3b68260413c579d2d7
Polina Stoyanova <polina.a.stoyanova@gmail.com>
2015-03-14 00:48:01 +0000

    Fixes to view dashboards files for both staff and student views

--- staff/viewDashboards.php
+++ staff/viewDashboards.php
@@ -131,37 +131,46 @@ foreach ($getStaffDetails as $staffDetail) {
 
 <div class="container">
 
-  <h1>View dashboards</h1>
-
-<form action="staffDashboard.php" method="get">
-<select id="staff" name="staff">
-<option value="0">Please select a staff</option>
-<?php
-foreach($getAllUnauthorisedStaffs as $getAllUnauthorisedStaff){
-    echo '<option value="' . $getAllUnauthorisedStaff['staff_id'] . '">' . $getAllUnauthorisedStaff['staff_first'] . " " . $getAllUnauthorisedStaff['staff_last'] . "</option>";
-}
-?>
-</select>
-
-<input type="submit" id="staffSubmit" onclick="return validateStaffSearch();" name="staffSubmit" value="View staff dashboard">
-</form>
-
-
-<form action="studentDashboard.php" method="get">
-<select id="student" name="student">
-<option value="0">Please select a student</option>
-<?php
-foreach($getAllProjectStudents as $getAllProjectStudent){
-    echo '<option value="' . $getAllProjectStudent['student_id'] . '">' . $getAllProjectStudent['student_first'] . " " . $getAllUnauthorisedStaff['student_last'] . "</option>";
-}
-?>
-</select>
-
-<input type="submit" id="studentSubmit" onclick="return validateStudentSearch();" name="studentSubmit" value="View student dashboard">
-</form>
-
-<div id="validation"></div>
-
+  <div class="row">
+        <div class="col s12">
+            <div class="card">
+                <div class="card-content">
+					<span class="card-title green-text">View dashboards</span>
+					<div class="row">
+						<form action="staffDashboard.php" method="get">
+						<div class="col s6">
+							<select id="staff" name="staff">
+							<option value="0">Please select a staff</option>
+							<?php
+							foreach($getAllUnauthorisedStaffs as $getAllUnauthorisedStaff){
+								echo '<option value="' . $getAllUnauthorisedStaff['staff_id'] . '">' . $getAllUnauthorisedStaff['staff_first'] . " " . $getAllUnauthorisedStaff['staff_last'] . "</option>";
+							}
+							?>
+							</select>
+						</div>
+						<button type="submit" id="staffSubmit" onclick="return validateStaffSearch();" name="staffSubmit" class="c_right-align waves-effect waves-teal waves-light green btn-flat white-text">View staff dashboard</button>
+						</form>
+					</div>
+					<div class="row">
+						<form action="studentDashboard.php" method="get">
+						<div class="col s6">
+							<select id="student" name="student">
+							<option value="0">Please select a student</option>
+							<?php
+							foreach($getAllProjectStudents as $getAllProjectStudent){
+								echo '<option value="' . $getAllProjectStudent['student_id'] . '">' . $getAllProjectStudent['student_first'] . " " . $getAllUnauthorisedStaff['student_last'] . "</option>";
+							}
+							?>
+							</select>
+						</div>
+						<button type="submit" id="studentSubmit" onclick="return validateStudentSearch();" name="studentSubmit" class="c_right-align waves-effect waves-teal waves-light green btn-flat white-text">View student dashboard</button>
+						</form>
+					</div>
+					<div id="validation"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <script type="text/javascript">
