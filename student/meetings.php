d4d365303d313f10d27d63b091a852d0dd0aca44
Mark Tickner <tm112@greenwich.ac.uk>
2015-03-28 09:52:27 +0000

    Improved server-side error messages

--- student/meetings.php
+++ student/meetings.php
@@ -330,7 +330,7 @@ if (count($errorList) > 0) {
 <div class="container">
 
     <!-- Output message text -->
-    <div class=" text-light-3 validation-error">
+    <div class="error" style="margin-top: 10px;">
         <?php echo $outputText; ?><?php echo $errorListOutput; ?>
     </div>
 
